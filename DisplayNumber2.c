f2ec6da434d9e95c5fcd7a3b374c159e1b958f12
Rutuja Dhawale <rutujadhawale749@gmail.com>
2025-05-19 02:34:19 +0530

    Assignment_6

--- DisplayNumber2.c
+++ DisplayNumber2.c
@@ -2,12 +2,58 @@
 
 void Display(int iNo)
 {
-    int iCnt=0;
-
-    for(iCnt = -iNo; iCnt<=iNo;iCnt++)
+    if(iNo<0)
     {
-        printf(" %d\n",iCnt);
+        iNo=-iNo;
     }
+    
+   switch(iNo)
+   {
+    case 0:
+        printf("Zero\n");
+        break;
+
+    case 1:
+        printf("One\n");
+        break;
+
+    case 2:
+        printf("Two\n");
+        break;
+
+    case 3:
+        printf("Three\n");
+        break;
+
+    case 4:
+        printf("Four\n");
+        break;
+
+    case 5:
+        printf("Five\n");
+        break;
+
+    case 6:
+        printf("Six\n");
+        break;
+
+    case 7:
+        printf("Seven\n");
+        break;
+
+    case 8:
+        printf("Eight\n");
+        break;
+
+    case 9:
+        printf("Nine\n");
+        break;
+
+    default:
+        printf("Invalid Number");
+        break;
+   } 
+    
 }
 
 int main()
@@ -21,5 +67,3 @@ int main()
 
     return 0;
 }
-
-//Time complexity 0(N)
---
+++ DisplayTable2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void TableRev(int iNo)
+{
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+    int iCnt=0;
+
+    for(iCnt=10; iCnt>=1;iCnt--)
+    {
+        printf( "%d\n",iNo*iCnt);
+    }
+
+
+}
+
+int main()
+{
+    int iValue=0;
+
+    printf("Enter Number:");
+    scanf("%d",&iValue);
+
+    TableRev(iValue);
+
+    return 0;
+
+}
+
+
+//Time complexity 0(N)
