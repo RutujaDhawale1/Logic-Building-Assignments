9baf265741b8542cbc660b10af35fb34126400cd
Rutuja Dhawale <rutujadhawale749@gmail.com>
2025-05-28 02:55:25 +0530

    Assignment_5

---
+++ Assignment_5/DisplayNumber.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+    int iCnt=0;
+
+    for(iCnt = 1; iCnt<=iNo;iCnt++)
+    {
+        printf("%d\n",iCnt);
+    }
+}
+
+int main()
+{
+    int iValue=0;
+
+    printf("Enter Number:");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
+//Time complexity 0(N)
