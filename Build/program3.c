a53486b9778a804de573eefa3db68baedbf6dfb3
Rutuja Dhawale <rutujadhawale749@gmail.com>
2026-03-29 02:23:52 +0530

    Add files via upload

---
+++ Build/program3.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+#define true 1
+#define false 0
+
+typedef int bool;
+typedef unsigned int UINT;
+
+bool ChkBit(UINT iNo)
+{
+    UINT iMask = 448;
+    UINT iResult = 0;
+
+    iResult = iNo & iMask;
+
+    return(iResult == iMask);
+}
+
+int main()
+{
+    UINT iValue = 0;
+    bool bRet = 0;
+
+    printf("Enter the number:");
+    scanf("%u", &iValue);
+
+    bRet = ChkBit(iValue);
+
+    if(bRet == true)
+    {
+        printf("7th and 8th and 9th Bit is ON\n");
+    }
+    else
+    {
+        printf(" either 7th or 8th or 9th bit is OFF or All ");
+    }
+
+    return 0;
+}
